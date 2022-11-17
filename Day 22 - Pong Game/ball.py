f6ff713a2fc84cfa31c9a6600f5aa6f526034f8b
qiaozern <zernkab@gmail.com>
2022-11-17 22:37:40 +0700

    add Day 22 project PongGame

---
+++ Day 22 - Pong Game/ball.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+import random
+
+class Ball(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+
+    def move(self):
+        x_pos = self.xcor() + self.x_move
+        y_pos = self.ycor() + self.y_move
+        self.goto(x_pos, y_pos)
+        
+    def bounce_y(self):
+        self.y_move *= -1 #reverse y direction
+        
+    def bounce_x(self):
+        self.x_move *= -1 #reverse x direction
+        self.move_speed *= 0.9
+        
+    def reset_position(self):
+        self.goto(0,0)
+        self.bounce_x()
+        self.move_speed = 0.1
